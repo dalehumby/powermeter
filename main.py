0a62996838bec5cdd3a014faa49102242fa6a1b5
Dale Humby <dale@humby.co.za>
2020-09-03 16:46:47 +0200

    Simple web server to turn pin on/off

--- main.py
+++ main.py
@@ -1,10 +1,72 @@
-from machine import RTC
-import ntptime
+import network
+import esp
+import gc
+import usocket as socket
+from machine import Pin
+import ujson
 
-# synchronize with ntp
-# need to be connected to wifi
-rtc = RTC()
-ntptime.settime()  # set the rtc datetime from the remote server
-t = rtc.datetime()  # get the date and time in UTC
+esp.osdebug(None)
 
-print(t)
+with open("config.json", "r") as f:
+    config = ujson.load(f.read())
+
+station = network.WLAN(network.STA_IF)
+station.active(True)
+station.connect(config["wifi"]["ssid"], config["wifi"]["password"])
+
+while not station.isconnected():
+    pass
+
+print("Connection successful")
+print(station.ifconfig())
+gc.collect()
+
+led = Pin(2, Pin.OUT)
+
+
+def web_page():
+    if led.value() == 1:
+        gpio_state = "ON"
+    else:
+        gpio_state = "OFF"
+
+    html = (
+        """<html><head> <title>ESP Web Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
+  h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none;
+  border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
+  .button2{background-color: #4286f4;}</style></head><body> <h1>ESP Web Server</h1>
+  <p>GPIO state: <strong>"""
+        + gpio_state
+        + """</strong></p><p><a href="/?led=on"><button class="button">ON</button></a></p>
+  <p><a href="/?led=off"><button class="button button2">OFF</button></a></p></body></html>"""
+    )
+    return html
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("", 80))
+s.listen(5)
+
+
+while True:
+    conn, addr = s.accept()
+    print("Got a connection from %s" % str(addr))
+    request = conn.recv(1024)
+    request = str(request)
+    print("Content = %s" % request)
+    led_on = request.find("/?led=on")
+    led_off = request.find("/?led=off")
+    if led_on == 6:
+        print("LED ON")
+        led.value(1)
+    if led_off == 6:
+        print("LED OFF")
+        led.value(0)
+    response = web_page()
+    conn.send("HTTP/1.1 200 OK\n")
+    conn.send("Content-Type: text/html\n")
+    conn.send("Connection: close\n\n")
+    conn.sendall(response)
+    conn.close()
+    gc.collect()
