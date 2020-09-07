b4f395b4ff1dffc703918ee35593097bd74355fa
Dale Humby <dale@humby.co.za>
2020-09-07 23:19:26 +0200

    Groundwork for keeping track of kWh
    Basic web server to display current kWh (GET), and to set kWh (POST)

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import network
-import esp
 import gc
+
+import esp
+import network
+import ujson
 import usocket as socket
 from machine import Pin
-import ujson
 
 esp.osdebug(None)
 
@@ -14,47 +15,69 @@ station.active(True)
 station.connect(config["wifi"]["ssid"], config["wifi"]["password"])
 while not station.isconnected():
     pass
-print("Connection successful")
+print("Connected to wifi")
 print(station.ifconfig())
 gc.collect()
 
-led = Pin(2, Pin.OUT)
+led = Pin(2, Pin.OUT)  # TODO replace with correct pin and handler function
 
 with open("index.html", "r") as f:
     index_template = f.read()
 
 
-def web_page():
-    if led.value() == 1:
-        gpio_state = "ON"
-    else:
-        gpio_state = "OFF"
-    return index_template.format(gpio_state=gpio_state)
+def handle_get():
+    """
+    Handle the http GET method
+
+    Retrieve the latest stats and return the rendered webpage
+    """
+    kwh = 100.3
+    return index_template.format(kwh=kwh)
+
+
+def handle_post(request):
+    """Handle the http POST method.
+
+    Get the request, pull out the kwh and persist
+    """
+    loc = request.find("kwh=")
+    param = request[loc:-1]
+    _, kwh = param.split("=")
+    return float(kwh)
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("", 80))
 s.listen(5)
+print("Ready for connections")
 
 
 while True:
     conn, addr = s.accept()
     print("Got a connection from %s" % str(addr))
-    request = conn.recv(1024)
-    request = str(request)
+    request = str(conn.recv(1024))
     print("Content = %s" % request)
-    led_on = request.find("/?led=on")
-    led_off = request.find("/?led=off")
-    if led_on == 6:
-        print("LED ON")
-        led.value(1)
-    if led_off == 6:
-        print("LED OFF")
-        led.value(0)
-    response = web_page()
-    conn.send("HTTP/1.1 200 OK\n")
-    conn.send("Content-Type: text/html\n")
+    if request.find("GET / ") >= 0:
+        print("Handle GET")
+        response = handle_get()
+        conn.send("HTTP/1.1 200 OK\n")
+        conn.send("Content-Type: text/html\n")
+    elif request.find("POST / ") >= 0:
+        print("Handle POST")
+        if request.find("kwh=") < 0:
+            # Browsers send a lot of headers, so try get more data if not found body yet
+            request = str(conn.recv(1024))
+        kwh = handle_post(request)
+        print(kwh)
+        conn.send("HTTP/1.1 301 Found\n")
+        conn.send("Location: /\n")
+        response = None
+    else:
+        print("Not found")
+        conn.send("HTTP/1.1 404 Not Found\n")
+        response = None
     conn.send("Connection: close\n\n")
-    conn.sendall(response)
+    if response:
+        conn.sendall(response)
     conn.close()
     gc.collect()
